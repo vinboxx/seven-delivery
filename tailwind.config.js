60b24baeef2f77fcf7620e160100f4ef3f2620f1
vinboxx <vinboxx@gmail.com>
2021-01-23 17:26:37 +0700

    Add Tailwind and TypeScript

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    purge: [
+        './src/pages/**/*.js',
+        './src/components/**/*.js'
+    ],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
