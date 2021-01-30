83a2954249d9318101a8ef1b134b330009314dcd
vinboxx <vinboxx@gmail.com>
2021-01-31 00:26:39 +0700

    Fix firebase config

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,7 +1,9 @@
 import firebase from "firebase";
 
-const config = {
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-};
+const config = process.env.FIREBASE_CONFIG ?
+    JSON.parse(process.env.FIREBASE_CONFIG)
+    : {
+        databaseURL: process.env.FIREBASE_DATABASE_URL,
+    };
 
 export default !firebase.apps.length ? firebase.initializeApp(config) : firebase.app();
