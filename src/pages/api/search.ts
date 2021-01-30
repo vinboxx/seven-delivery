2cc063c1497602b8eb9f18b2a481dface3983a4b
vinboxx <vinboxx@gmail.com>
2021-01-31 02:30:34 +0700

    Add GraphQL

--- src/pages/api/search.ts
+++
@@ -1,9 +0,0 @@
-import firebase from '../../lib/firebase';
-
-const database = firebase.database();
-
-export default async (req, res) => {
-    const snapshot = await database.ref('/products').once('value');
-
-    res.json(snapshot.val());
-}
